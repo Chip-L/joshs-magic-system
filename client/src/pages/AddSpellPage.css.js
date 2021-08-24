aea6a59bcc837f8d8059c6206f0b9510c81367c6
Chip Long <chip_long@yahoo.com>
2021-08-24 15:08:11 -0600

    added some basic formats and components to css. started form

--- client/src/pages/AddSpellPage.css.js
+++ client/src/pages/AddSpellPage.css.js
@@ -1,1 +1,43 @@
 import styled from "styled-components";
+
+export const Title = styled.h2`
+  margin-bottom: 1rem;
+`;
+
+export const Section = styled.section`
+  border-top: 1px solid black;
+  border-bottom: 1px solid black;
+  background-color: rgba(251, 245, 180, 0.3);
+`;
+
+export const Form = styled.form`
+  // form styling here
+`;
+
+export const Label = styled.label`
+  padding-right: 0.5rem;
+  align-text: right;
+
+  &::after {
+    content: ":";
+  }
+`;
+
+export const Control = styled.input`
+  // input field formatting here
+`;
+
+export const Button = styled.input`
+  background-color: blue;
+  color: white;
+  margin-top: 0.5rem; padding 1.4rem 5rem;
+  border: none;
+  border-radius: 0.4rem;
+  cursor: pointer;
+  font-weight: bold;
+  font-size: 1.6rem;
+
+  &:hover {
+    background-color: lightblue;
+  }
+`;
--- client/src/pages/AddSpellPage.js
+++ client/src/pages/AddSpellPage.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { Form, Label, Section, Title } from "./AddSpellPage.css";
 
 const AddSpellPage = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log("you submitted");
+  };
   return (
     <>
-      <p>Add Spell</p>
+      <Title>Add Spell</Title>
+      <Form onSubmit={handleSubmit}>
+        <Section>
+          <Label>Spell Name</Label>
+        </Section>
+      </Form>
     </>
   );
 };
