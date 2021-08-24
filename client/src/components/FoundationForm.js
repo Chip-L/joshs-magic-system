dadbad506cb9bf078c81005d65bbf412c0c47ef9
Chip Long <chip_long@yahoo.com>
2021-08-24 16:17:33 -0600

    set up handlechange for foundation

--- client/src/components/FoundationForm.js
+++ client/src/components/FoundationForm.js
@@ -1,24 +1,39 @@
 import React, { useState } from "react";
 import { Control, Form, Label, Section } from "../pages/AddSpellPage.css";
 
-const FoundationForm = () => {
+/**
+ * displays the spell Foundation form. State is tracked in the parent. If values are passed in, they should be specifically for the foundation.
+ *
+ * values = foundation.*
+ * update = function used to set the state.
+ */
+const FoundationForm = ({ values, update }) => {
   // const [foundationState, setFoundationState] = useState();
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    const foundation = { ...values.foundation };
+    foundation[name] = value;
+
+    update({
+      ...values,
+      foundation,
+    });
+  };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Section className="foundation">
-        <Label htmlFor="spellName">Spell Name</Label>
-        <Control
-          type="text"
-          required
-          id="spellName"
-          name="spellName"
-          onChange={handleChange}
-        />
-        <Label></Label>
-        <Label>Traits</Label>
-      </Section>
-    </Form>
+    <Section className="foundation">
+      <Label htmlFor="spellName">Spell Name</Label>
+      <Control
+        type="text"
+        required
+        id="spellName"
+        name="spellName"
+        value={values.spellName}
+        onChange={handleChange}
+      />
+      <Label>Mana</Label>
+      <Label>Traits</Label>
+    </Section>
   );
 };
 
--- client/src/pages/AddSpellPage.js
+++ client/src/pages/AddSpellPage.js
@@ -1,20 +1,24 @@
-import React from "react";
-import FoundationForm from "../components/FSoundationForm";
-import { Button, Title } from "./AddSpellPage.css";
+import React, { useState } from "react";
+import FoundationForm from "../components/FoundationForm";
+import { Button, Form, Title } from "./AddSpellPage.css";
 
 const AddSpellPage = () => {
   const [foundationState, setFoundationState] = useState({});
 
   const handleSubmit = (event) => {
-    event.preventDefault();
+    // event.preventDefault();
 
-    console.log("you submitted");
+    console.log("you submitted:");
+    console.log(JSON.stringify(foundationState));
   };
   return (
     <>
       <Title>Add Spell</Title>
-      <FoundationForm values={foundationState} update={setFoundationState} />
-      <Button type="submit" value="Submit" />
+      <Form onSubmit={handleSubmit}>
+        <FoundationForm values={foundationState} update={setFoundationState} />
+        <Button type="submit" value="Submit" />
+      </Form>
+      <p>{JSON.stringify(foundationState)}</p>
     </>
   );
 };
