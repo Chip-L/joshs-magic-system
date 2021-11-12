0b9f91cece1857b0ac7ad658b828b74e53cd4cd3
clong desktop <chip_long@yahoo.com>
2021-11-12 01:23:13 -0700

    CheckBoxGroup is now functioning!

--- client/src/components/CheckboxGroup.js
+++ client/src/components/CheckboxGroup.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CheckboxGroup({ name, valueList, values, update }) {
+  const [arrChecked, setArrChecked] = useState(values?.[name] || []);
+
+  const handleChange = (value) => {
+    console.log(arrChecked, value);
+    //the logic here is to remove the id if its already exist else add it. and set it back to state
+    const newArrChecked = arrChecked.includes(value)
+      ? arrChecked.filter((i) => i != value)
+      : [...arrChecked, value];
+
+    setArrChecked(newArrChecked);
+    update({
+      ...values,
+      [name]: newArrChecked,
+    });
+  };
+
   return (
     <>
       {valueList.map((item) => (
@@ -9,15 +25,16 @@ function CheckboxGroup({ name, valueList, values, update }) {
             type="checkbox"
             id={`${name}_${item}`}
             name={name}
+            checked={arrChecked.includes(item)}
             value={item}
-            // key={`${name}_${item}`}
+            onChange={() => handleChange(item)}
           />
           <label
             htmlFor={`${name}_${item}`}
             className="rightLabel"
             key={`${name}_${item}`}
           >
-            Arcane
+            {item}
           </label>
         </div>
       ))}
